6c857c0327d74dc171f011d27f404df8f4183503
Jacob <70616107+CS-Cowboy@users.noreply.github.com>
2023-09-11 21:35:14 -0500

    Changes
    Changed how things are stored and computed and organized things a bit more. Removed unnecessary code.

--- earnings_calculator.py
+++ earnings_calculator.py
@@ -2,44 +2,64 @@ import sys as sys
 import re as re
 from PyPDF2 import PdfReader
 
-def getCreditQuantity(line, match_target, boo):
+def containsCredit(line):
     match_credit = re.compile(r'Credit:.+$')
+    return (match_credit.search(line) is not None)
+
+def containsTip(line):
+    match_tips = re.compile(r'Tips.+$')
+    found = match_tips.search(line)
+    return found is not None
+
+def containsReimbursement(line):
+    match_credit = re.compile(r'Reimbursement.+$')
+    found = match_credit.search(line)
+    return found is not None
+
+def getQuantity(line):
     match_quantity = re.compile(r'[0-9]+\.[0-9]+')
+    return float(match_quantity.search(line).group())
 
-    search_regexp = lambda string, compiled_regexp : compiled_regexp.search(string)
-
-
-    found_content =  search_regexp(line, match_credit)
-    if(found_content is not None):
-        found_target = search_regexp(line, match_target)
-        if(found_target is not None):
-            value = search_regexp(line, match_quantity).group()
-            #if (boo is False):
-            #    print("Fuel:\t", value)
-            #else:
-            #    print("Tip Val=\t\t\t", value)
-            return float(value)
-    return float(0)
+def printStatement(data, otput):
+    print("Itemized breakdown:\n")
+    for group in data:
+        print(group, "\t:=", data[group], "\n")
+    print("Total:=\t", otput)
 
 if len(sys.argv) == 2:
     
     f = open(str(sys.argv[1]), 'rb')
     reader = PdfReader(f)
-    earnings = float(0)
-    reimbursements = float(0)
-    match_tips = re.compile(r'Tips.+$')
-    match_credit = re.compile(r'Reimbursement.+$')
+    totalEarns = float(0)
+    totalReimb = float(0)
     quantities = 0
+    days = 1
+    week_no = 1
+    reimbursements = dict({})
+    earnings = dict({})
     for page in reader.pages:
         lines = page.extract_text().split('\n')
         for text in lines:
-            earnings += getCreditQuantity(text, match_tips, True)
-            reimbursements += getCreditQuantity(text, match_credit, False)
-    print("Total earnings:\n", (reimbursements + earnings))
-    print("Breakdown:\n")
-    print('Reimbursements:\t', reimbursements)
-    print('Tips:\t', earnings)
-
+            if(containsCredit(text)):
+                if((days % 15) == 0 or days == 1):
+                    week = 'week_' +str(week_no)
+                    reimbursements.update({week : []})
+                    earnings.update({week : []})
+                    week_no += 1
+                    days = 1
+                val = getQuantity(text)
+                if(containsReimbursement(text)):
+                    reimbursements[week].append(val)
+                    totalReimb += val
+                if(containsTip(text)):
+                    earnings[week].append(val)
+                    totalEarns += val
+                days+=1
+                
+    print("EARNINGS:\n")
+    printStatement(earnings, totalEarns)
+    print("REIMBURSED:\n")
+    printStatement(reimbursements, totalReimb)
     f.close()
 else:
     print('usage: python earnings_calculator.py <pdf_to_read>\n')
