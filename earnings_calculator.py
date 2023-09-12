63685025791fa80909fd305a6b13017803c4228b
Jacob <70616107+CS-Cowboy@users.noreply.github.com>
2023-09-11 22:07:28 -0500

    More changes
    A few more changes to organization, output formatting, and data computation.

--- earnings_calculator.py
+++ earnings_calculator.py
@@ -20,46 +20,48 @@ def getQuantity(line):
     match_quantity = re.compile(r'[0-9]+\.[0-9]+')
     return float(match_quantity.search(line).group())
 
-def printStatement(data, otput):
-    print("Itemized breakdown:\n")
-    for group in data:
-        print(group, "\t:=", data[group], "\n")
-    print("Total:=\t", otput)
+def printStatement(data):
+    print("\tItemized breakdown:\n")
+    month = list([])
+    for key in data:
+        print("\t\t", key, ":=  ", data[key])
+        weekly_total = sum(data[key])
+        month.append(weekly_total)
+        print("\t\tWeek total:=  ", weekly_total, "\n\n\t\t---------------------------\n")
+    months_total = sum(month)
+    print("\tMonths Total:=\t\t", months_total)
+    return months_total     #return for complete total.
 
 if len(sys.argv) == 2:
     
     f = open(str(sys.argv[1]), 'rb')
     reader = PdfReader(f)
-    totalEarns = float(0)
-    totalReimb = float(0)
-    quantities = 0
     days = 1
-    week_no = 1
-    reimbursements = dict({})
-    earnings = dict({})
+    week = 'week_' + str(1)
+    reimbursements = dict({week : []})
+    earnings = dict({week : []})
     for page in reader.pages:
         lines = page.extract_text().split('\n')
         for text in lines:
             if(containsCredit(text)):
-                if((days % 15) == 0 or days == 1):
-                    week = 'week_' +str(week_no)
+                if((days % 15) == 0):
+                    week = 'week_' + str(len(earnings)+1)
                     reimbursements.update({week : []})
                     earnings.update({week : []})
-                    week_no += 1
                     days = 1
                 val = getQuantity(text)
                 if(containsReimbursement(text)):
                     reimbursements[week].append(val)
-                    totalReimb += val
                 if(containsTip(text)):
                     earnings[week].append(val)
-                    totalEarns += val
-                days+=1
                 
-    print("EARNINGS:\n")
-    printStatement(earnings, totalEarns)
-    print("REIMBURSED:\n")
-    printStatement(reimbursements, totalReimb)
+                days+=1
+    print("\nEARNINGS  follow:\n")
+    monthsTotalTips = printStatement(earnings)
+    print("\nREIMBURSED\tfollows:\n")
+    monthsTotalEarns = printStatement(reimbursements)
+    print("COMPLETE MONTHS TOTAL=\t", (monthsTotalEarns + monthsTotalTips))
     f.close()
+    
 else:
     print('usage: python earnings_calculator.py <pdf_to_read>\n')
